606392f7f13dd831d0128c141273f9f245b3df52
konpaleo <konpaleo@hotmail.com>
2025-06-08 19:18:25 +0300

    Updates config file

--- app/core/config.py
+++ app/core/config.py
@@ -15,14 +15,4 @@ class ApplicationSettings(BaseSettings):
 
     LOGGER_LEVEL: str | None = "INFO"
     OPEN_AI_API_KEY: str | None = "OPEN_AI_API_KEY"
-    WTOC_API_KEY: str | None = "API-KEY"
-    WTOC_API_URL: str | None = "http://localhost:3000"
-
-    TASK_QUEUE: str | None = "wtoc-tasks"
-
-    REDIS_PASSWORD: str | None = "password"
-    REDIS_SERVER: str | None = "localhost"
-    REDIS_PORT: int | None = 6379
-
-    GOOGLE_SEARCH_API_KEY: str | None = "GOOGLE-SEARCH-API-KEY"
-    GOOGLE_SEARCH_ENGINE_ID: str | None = "GOOGLE-SEARCH-ENGINE-ID"
+    API_URL: str | None = "http://localhost:3000"
